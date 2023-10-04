e1d4a4aca31b41cf5659f8983e2f77e896d71402
Oscar <oscarwruedakollmer@gmail.com>
2023-10-04 20:10:26 +0200

    Implementacion del registro

--- lib/OnBoarding/RegisterView.dart
+++ lib/OnBoarding/RegisterView.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
-class RegisterView extends StatelessWidget{
+import '../Custom/CustomAppBar.dart';
+import '../Custom/CustomButton.dart';
+import '../Custom/CustomTextFormField.dart';
+
+class RegisterView extends StatelessWidget {
+
+  late BuildContext _context;
+  TextEditingController tecUsername=TextEditingController();
+  TextEditingController tecPassword=TextEditingController();
+  TextEditingController tecRepass=TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Text('Esta es la pagina de registro');
+    this._context=context;
+    return Scaffold(
+      appBar: CustomAppBar(sTitulo: 'Bienvendio al registro'),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CustomTextFormField(tecController: tecUsername, sLabel: 'Escribe tu usuario'),
+            CustomTextFormField(tecController: tecPassword, sLabel: 'Escribe tu contraseña', blIsPassword: true),
+            CustomTextFormField(tecController: tecPassword, sLabel: 'Vuelva a escribir su contraseña', blIsPassword: true),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CustomButton(sNombre: 'Cancelar', onPressed: onClickCancelar),
+                  CustomButton(sNombre: 'Aceptar', onPressed: onClickAceptar),
+                ]
+            )
+          ]
+      ),
+    );
   }
 
+  void onClickAceptar(){}
+
+  void onClickCancelar(){}
 }
