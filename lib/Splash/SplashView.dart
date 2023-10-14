8daea429d53577d4970bb89fe24933e84dcd7437
Oscar <oscarwruedakollmer@gmail.com>
2023-10-14 12:27:47 +0200

    Logo sin fondo y relojito de carga

--- lib/Splash/SplashView.dart
+++ lib/Splash/SplashView.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget{
+class SplashView extends StatelessWidget {
+  late BuildContext _context;
+
+  void checkSession() async {}
+
   @override
   Widget build(BuildContext context) {
+    _context = context;
 
-    return Image.asset("resources/kyty_logo.png");
+    Column column = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset("resources/kyty_logo.png", width: 300, height: 450),
+        CircularProgressIndicator(
+          color: Colors.white,
+          backgroundColor: Colors.cyanAccent,
+          strokeWidth: 6.0, // Ajusta el grosor del indicador
+        )
+      ],
+    );
+
+    return Center(child: column);
   }
-  
 }
