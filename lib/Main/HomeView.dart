6321f86fb8e2a89125946a73fd81f308d46ec002
Oscar <oscarwruedakollmer@gmail.com>
2023-10-16 20:57:54 +0200

    Home con mensaje de bienvenida personalizado, cambio de stateles a estateful

--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -1,12 +1,37 @@
 import 'package:actividad1/Custom/CustomAppBar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget{
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  String username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    cargarPerfil();
+  }
+
+  void cargarPerfil() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    DocumentSnapshot<Map<String, dynamic>> perfil = await db.collection("Users").doc(uid).get();
+
+    setState(() {
+      username = perfil.data()?["username"];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade50,
-      appBar: CustomAppBar(sTitulo: 'Bienvenido al Home'),
+      appBar: CustomAppBar(sTitulo: 'Bienvenid@ $username'),
     );
   }
 }
